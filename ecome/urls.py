f0e015f11131603a95fa55defb738dea39232ce3
sihu786 <adakb554@gmail.com>
2024-09-21 23:33:46 +0530

    first commit

--- ecome/urls.py
+++ ecome/urls.py
@@ -19,6 +19,8 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Ecome Admin "
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('afilate.urls')),
